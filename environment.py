b8259a69962de91d7534b21d5a692bc14e8487a5
kywch <choe.kyoung@gmail.com>
2024-03-15 21:01:40 -0700

    removed limited reward, added menu reward with cooldown

--- environment.py
+++ environment.py
@@ -13,6 +13,8 @@ from pokemonred_puffer.environment import (
 
 MUSEUM_TICKET = (0xD754, 0)
 
+MENU_COOLDOWN = 300
+
 
 class CustomRewardEnv(RedGymEnv):
     def __init__(self, env_config: pufferlib.namespace, reward_config: pufferlib.namespace):
@@ -75,16 +77,21 @@ class CustomRewardEnv(RedGymEnv):
         self.max_event_rew = 0
         self.max_level_sum = 0
         self.use_limited_reward = False
-        self.menu_reward_cooldown = 0  # to prevent spamming limit reward
 
         # KEY events
         self.bill_said_use_cell_separator = False
         self.got_hm01 = False
 
+        # Track action bag menu
+        self.action_bag_menu_count = 0
+        self.rewarded_action_bag_menu = 0
+        self.menu_reward_cooldown = 0  # to prevent spamming limit reward
+
         # Track learn moves with item
         self.curr_moves = 0
         self.curr_item_num = 0
         self.moves_learned_with_item = 0
+        self.just_learned_item_move = 0
 
         self._update_event_obs(reset=True)
         self.base_event_flags = self.event_obs.sum()
@@ -97,12 +104,21 @@ class CustomRewardEnv(RedGymEnv):
         self.bill_said_use_cell_separator = self.read_bit(0xD7F2, 6)
         self.got_hm01 = self.read_bit(0xD803, 0)
 
-        # Check learn moves with item -- could be spammed later, but it's fine for now
         if not reset:
+            # Check action bag menu
+            if self.seen_action_bag_menu > 0:
+                self.action_bag_menu_count += 1
+                if self.menu_reward_cooldown == 0:
+                    self.menu_reward_cooldown = MENU_COOLDOWN
+                    self.rewarded_action_bag_menu += 1
+
+            # Check learn moves with item -- could be spammed later, but it's fine for now
             new_moves = self.moves_obtained.sum()
+            self.just_learned_item_move = 0
             if new_moves > self.curr_moves:  # move learned
                 if self.read_m(0xD31D) < self.curr_item_num:  # item consumed
                     self.moves_learned_with_item += 1
+                    self.just_learned_item_move = 1
             self.curr_moves = new_moves
             self.curr_item_num = self.read_m(0xD31D)
 
@@ -110,7 +126,7 @@ class CustomRewardEnv(RedGymEnv):
         if self.menu_reward_cooldown > 0:
             self.menu_reward_cooldown -= 1
 
-        self.use_limited_reward = self.got_hm01_cut_but_not_learned_yet()
+        #self.use_limited_reward = self.got_hm01_cut_but_not_learned_yet()
         # if self.use_limited_reward:
         #     # NOTE: only for HM cut now
         #     self.set_cursor_to_item(target_id=0xC4)  # 0xC4: HM cut
@@ -124,20 +140,25 @@ class CustomRewardEnv(RedGymEnv):
         if "stats" in info:
             info["stats"]["under_limited_reward"] = self.use_limited_reward
             info["stats"]["learn_with_item"] = self.moves_learned_with_item
+            info["stats"]["action_bag_menu_count"] = self.action_bag_menu_count
+            info["stats"]["rewarded_action_bag_menu"] = self.rewarded_action_bag_menu
 
         return obs, rew, reset, False, info
 
     # Reward is computed with update_reward(), which calls get_game_state_reward()
     def update_reward(self):
 
-        # if has hm01 cut, then do not give normal reward until cut is learned
-        if self.use_limited_reward:
-            # encourage going to action bag menu with very small reward
-            if self.seen_action_bag_menu is True and self.menu_reward_cooldown == 0:
-                self.menu_reward_cooldown = 30
-                return 0.0001
+        # # if has hm01 cut, then do not give normal reward until cut is learned
+        # if self.use_limited_reward:
+        #     if self.just_learned_item_move > 0:  # encourage any learning from item
+        #         return 0.1
 
-            return 0
+        #     # encourage going to action bag menu with very small reward
+        #     if self.seen_action_bag_menu is True and self.menu_reward_cooldown == 0:
+        #         self.menu_reward_cooldown = 30
+        #         return 0.0001
+
+        #     return 0
 
         # compute reward
         self.progress_reward = self.get_game_state_reward()
@@ -147,21 +168,21 @@ class CustomRewardEnv(RedGymEnv):
         self.total_reward = new_total
         return new_step
 
-    def got_hm01_cut_but_not_learned_yet(self):
-        # prev events that need to be true
-        prev_events = [
-            self.read_bit(0xD7F1, 0),  # met bill
-            self.read_bit(0xD7F2, 3),  # used cell separator on bill
-            self.read_bit(0xD7F2, 4),  # ss ticket
-            self.read_bit(0xD7F2, 5),  # met bill 2
-            self.read_bit(0xD7F2, 6),  # bill said use cell separator
-            self.read_bit(0xD7F2, 7),  # left bills house after helping
-        ]
+    # def got_hm01_cut_but_not_learned_yet(self):
+    #     # prev events that need to be true
+    #     prev_events = [
+    #         self.read_bit(0xD7F1, 0),  # met bill
+    #         self.read_bit(0xD7F2, 3),  # used cell separator on bill
+    #         self.read_bit(0xD7F2, 4),  # ss ticket
+    #         self.read_bit(0xD7F2, 5),  # met bill 2
+    #         self.read_bit(0xD7F2, 6),  # bill said use cell separator
+    #         self.read_bit(0xD7F2, 7),  # left bills house after helping
+    #     ]
 
-        got_hm01 = self.read_bit(0xD803, 0)
-        rubbed_captain = self.read_bit(0xD803, 1)
+    #     got_hm01 = self.read_bit(0xD803, 0)
+    #     rubbed_captain = self.read_bit(0xD803, 1)
 
-        return all(prev_events) and got_hm01 and rubbed_captain and not self.taught_cut
+    #     return all(prev_events) and got_hm01 and rubbed_captain and not self.taught_cut
 
     # TODO: make the reward weights configurable
     def get_game_state_reward(self, print_stats=False):
@@ -197,6 +218,7 @@ class CustomRewardEnv(RedGymEnv):
             "explore_npcs": sum(self.seen_npcs.values()) * 0.03,  # talk to new npcs
             "explore_hidden_objs": sum(self.seen_hidden_objs.values()) * 0.02,  # look for new hidden objs
             "moves_obtained": self.curr_moves * 0.01,  # try to learn new moves, via menuing?
+            "action_bag_menu": self.rewarded_action_bag_menu * 0.001,  # try to use items, via menuing?
 
             # Cut-related. Revisit later.
             "cut_coords": sum(self.cut_coords.values()) * 1.0,
