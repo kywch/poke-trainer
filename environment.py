32d0ca7ff8d95d1375fe605fc522eb51a2acde1b
kywch <choe.kyoung@gmail.com>
2024-03-18 21:50:10 -0700

    renamed explore to explore_tile

--- environment.py
+++ environment.py
@@ -240,7 +240,7 @@ class CustomRewardEnv(RedGymEnv):
             # NOTE: exploring "newer" tiles is the main driver of progression
             # Visit decay makes the explore reward "dense" ... little reward everywhere
             # so agents are motivated to explore new coords and/or revisit old coords
-            "explore": self.tile_reward * 0.01,
+            "explore_tile": self.tile_reward * 0.01,
 
             # First, always search for new pokemon and events
             "seen_pokemon": self.seen_pokemon.sum() * 1.5,  # more related to story progression?
